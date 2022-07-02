c341e1cea5296116ec80ac94f39f86b9dacd5307
Tom Pan <tommmpn@gmail.com>
2022-07-02 19:07:30 -0400

    Update

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu')
+const menuLinks = document.querySelector('.navbar__menu');
+
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
 
+window.addEventListener('scroll', function() {
+    const toTop = document.querySelector('.to-top');
+        toTop.classList.toggle('active', window.scrollY > 150);
+});
