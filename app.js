1d8d0dbb6d833cc2df7926eb7576040ecfc6e8d6
Tom Pan <tommmpn@gmail.com>
2022-07-10 17:54:20 -0400

    Update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
+var icon = document.getElementById("change__icon");
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
@@ -11,3 +12,28 @@ window.addEventListener('scroll', function() {
     const toTop = document.querySelector('.to-top');
         toTop.classList.toggle('active', window.scrollY > 150);
 });
+
+if (localStorage.getItem("theme") == null) {
+    localStorage.setItem("theme", "light");
+} 
+
+let localData = localStorage.getItem("theme");
+
+if (localData == "light") {
+    icon.src = "images/moon.png";
+    document.body.classList.remove("dark-theme");
+} else if (localData = "dark") {
+    icon.src = "images/sun.png";
+    document.body.classList.add("dark-theme");
+}
+
+icon.onclick = function() {
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        icon.src = "images/sun.png";
+        localStorage.setItem("theme", "dark");
+    } else {
+        icon.src = "images/moon.png";
+        localStorage.setItem("theme", "light");
+    }
+}
